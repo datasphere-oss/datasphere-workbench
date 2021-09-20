708052167f4a726a20d509e3cd2dad110478a667
theseusyang <theseusyang@gmail.com>
2021-09-20 20:57:05 +0800

    add apache license

---
+++ datasource-service/src/main/java/com/huahui/datasphere/workbench/datasource/model/MongoDBConnectionModel.java
@@ -0,0 +1,141 @@
+/*
+ * Apache License
+ * 
+ * Copyright (c) 2020 HuahuiData
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.huahui.datasphere.workbench.datasource.model;
+
+import java.io.Serializable;
+
+import com.huahui.datasphere.datasphere.workbench.common.vo.DataSourceMetadata;
+
+public class MongoDBConnectionModel implements Serializable{
+	
+	private static final long serialVersionUID = 913920227993681273L;
+	
+	private String hostname;
+	private int port;
+	private String username;
+	private String password;
+	private String dbName;
+	private String collectionName;
+	private DataSourceMetadata metadata;
+
+	/**
+	 * @return the hostname
+	 */
+	public String getHostname() {
+		return hostname;
+	}
+
+	/**
+	 * @param hostname the hostname to set
+	 */
+	public void setHostname(String hostname) {
+		this.hostname = hostname;
+	}
+
+	/**
+	 * @return the port
+	 */
+	public int getPort() {
+		return port;
+	}
+
+	/**
+	 * @param port the port to set
+	 */
+	public void setPort(int port) {
+		this.port = port;
+	}
+
+	/**
+	 * @return the username
+	 */
+	public String getUsername() {
+		return username;
+	}
+
+	/**
+	 * @param username the username to set
+	 */
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	/**
+	 * @return the password
+	 */
+	public String getPassword() {
+		return password;
+	}
+
+	/**
+	 * @param password the password to set
+	 */
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	/**
+	 * @return the dbName
+	 */
+	public String getDbName() {
+		return dbName;
+	}
+
+	/**
+	 * @param dbName the dbName to set
+	 */
+	public void setDbName(String dbName) {
+		this.dbName = dbName;
+	}
+
+	/**
+	 * @return the collectionName
+	 */
+	public String getCollectionName() {
+		return collectionName;
+	}
+
+	/**
+	 * @param collectionName the collectionName to set
+	 */
+	public void setCollectionName(String collectionName) {
+		this.collectionName = collectionName;
+	}
+
+	/**
+	 * @return the metadata
+	 */
+	public DataSourceMetadata getMetadata() {
+		return metadata;
+	}
+
+	/**
+	 * @param metadata the metadata to set
+	 */
+	public void setMetadata(DataSourceMetadata metadata) {
+		this.metadata = metadata;
+	}
+
+
+}
