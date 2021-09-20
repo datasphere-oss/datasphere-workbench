828cc7924f061229a53b42799e6c16d37da36a90
theseusyang <theseusyang@gmail.com>
2021-09-20 20:26:58 +0800

    add DataSourceServiceApplication

---
+++ datasource-service/src/main/java/com/huahui/datasphere/workbench/datasource/DataSourceServiceApplication.java
@@ -0,0 +1,53 @@
+/*
+ * Apache License
+ * 
+ * Copyright (c) 2020 HuahuiData
+ * 
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
+package com.huahui.datasphere.workbench.datasource;
+
+import java.lang.invoke.MethodHandles;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+@SpringBootApplication
+@EnableAutoConfiguration(exclude = {DataSourceAutoConfiguration.class})
+public class DataSourceServiceApplication {
+	private static final Logger logger = LoggerFactory.getLogger(MethodHandles.lookup().lookupClass());
+
+	public static final String CONFIG_ENV_VAR_NAME = "SPRING_APPLICATION_JSON";
+
+	public static void main(String[] args) {
+
+		final String springApplicationJson = System.getenv(CONFIG_ENV_VAR_NAME);
+		if (springApplicationJson != null && springApplicationJson.contains("{")) {
+			logger.debug("main: successfully parsed configuration from environment {}", CONFIG_ENV_VAR_NAME);
+		} else {
+			logger.warn("main: no configuration found in environment {}", CONFIG_ENV_VAR_NAME);
+		}
+		SpringApplication.run(DataSourceServiceApplication.class, args);
+	}
+
+}
